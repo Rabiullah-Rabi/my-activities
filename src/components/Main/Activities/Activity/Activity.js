7134b56832971c3db74e1ddee7f01ff90c1786f2
Rabiullah-Rabi <mdrabiullah789@gmail.com>
2022-09-28 22:25:03 +0600

    design activity card

--- src/components/Main/Activities/Activity/Activity.js
+++ src/components/Main/Activities/Activity/Activity.js
@@ -2,10 +2,13 @@ import React from 'react';
 import './Activity.css'
 const Activity = ({ activity }) => {
     const { name, img, duration } = activity;
+
     return (
         <div className='activity-wrapper'>
             <img src={img}  className='thumbnail' alt="" />
-            <h1>{name}</h1>
+            <h3>{name}</h3>
+            <p>Duration: {duration} min</p>
+            <button className='add-to-list'>Add To list</button>
         </div>
     );
 };
