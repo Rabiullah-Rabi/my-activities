1c73f5f1e1552a91cffd277d7ca6db976685a594
Rabiullah-Rabi <mdrabiullah789@gmail.com>
2022-09-29 19:39:54 +0600

    update faq answer

--- src/components/Main/Faq/Faq.js
+++ src/components/Main/Faq/Faq.js
@@ -28,8 +28,7 @@ const Faq = () => {
                         <li>Running on state change: validating input field</li>
                         <li>Running on state change: live filtering</li>
                         <li>Running on state change: trigger animation on new array value</li>
-                        <li>Running on props change: update paragraph list on fetched API data update</li>
-                        <li>Running on props change: updating fetched API data to get BTC updated price</li>
+                        <li>Running on props change: updating fetched API data</li>
                     </ul>
                 </p>
             </div>
